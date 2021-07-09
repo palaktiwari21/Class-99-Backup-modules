210b7dbeb702bd6c71007df9af253ee46a734f2d
palaktiwari21 <67467227+palaktiwari21@users.noreply.github.com>
2021-07-09 15:57:16 +0530

    Add files via upload

---
+++ 99.py
@@ -0,0 +1,9 @@
+import os
+import shutil
+source=input("Enter Source Folder Name : ")
+destination=input("Enter Destination Folder Name : ")
+source=source+"/"
+destination=destination+"/"
+list_of_files=os.listdir(source)
+for file in list_of_files:
+    shutil.move((source+file),destination)
